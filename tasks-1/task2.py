e83b0420ac878951273da5c1048791273c89a5af
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-08-02 17:40:51 +0300

    solved task2

---
+++ tasks-1/task2.py
@@ -0,0 +1,13 @@
+arr = [1, 3, 2, 5, 3, 4]
+
+# out = [5, 5, 5, 0, 4, 0]
+
+for i in range(len(arr)):
+    m = max(arr[i:])
+    if arr[i] < m:
+        arr[i] = m
+    else:
+        arr[i] = 0
+        
+print(arr)
+        
