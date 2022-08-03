eee47c327b12270f5e05cc344c5add6466e25162
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-08-03 18:29:36 +0300

    removed sets

--- tasks-1/task3.py
+++ tasks-1/task3.py
@@ -1,11 +1,21 @@
+from itertools import chain
+
+
 counter = 0
 n = 1000
 
 # output = 233168
-l1 = set(range(3, n+1, 3))
-l2 = set(range(5, n, 5))
-print(sum(set.union(l1, l2)))
-        
+l1 = list(range(3, n+1, 3))
+l2 = list(range(5, n, 5))
+      
+l3  = l1 + l2
 
-# counter = sum([i for i in range(n) if (i % 3 == 0) or (i % 5 == 0)])
-# print(counter)
+def unique(n):
+    u_list = []
+    for i in n:
+        if i not in u_list:
+            u_list.append(i)
+    return u_list
+
+
+print(sum(unique(l3)))
