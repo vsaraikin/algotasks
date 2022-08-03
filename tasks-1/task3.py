71e08abd03191d8a029a4fb4a00c0f643cbf676f
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-08-03 17:17:51 +0300

    increased the speed

--- tasks-1/task3.py
+++ tasks-1/task3.py
@@ -2,8 +2,10 @@ counter = 0
 n = 1000
 
 # output = 233168
-for i in range(n):
-    if (i % 3 == 0) or (i % 5 == 0):
-        counter += i
+l1 = set(range(3, n+1, 3))
+l2 = set(range(5, n, 5))
+print(sum(set.union(l1, l2)))
         
-print(counter)
+
+# counter = sum([i for i in range(n) if (i % 3 == 0) or (i % 5 == 0)])
+# print(counter)
