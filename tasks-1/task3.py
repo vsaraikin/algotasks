0ab24874e4b88ff68a7bf99a50e3cce9e2c70dd4
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-08-03 19:20:04 +0300

    solution using math sets

--- tasks-1/task3.py
+++ tasks-1/task3.py
@@ -4,15 +4,6 @@ n = 1000
 # output = 233168
 l1 = list(range(3, n+1, 3))
 l2 = list(range(5, n, 5))
-      
-l3  = l1 + l2
+l3 = list(range(15, n, 15))      
 
-def unique(n):
-    u_list = []
-    for i in n:
-        if i not in u_list:
-            u_list.append(i)
-    return u_list
-
-
-print(sum(unique(l3)))
+print(sum(l1) + sum(l2) - sum(l3))
