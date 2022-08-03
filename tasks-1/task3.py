999913d20abfd4956d51575e884368733b6d04d8
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-08-03 19:32:20 +0300

    removed lists

--- tasks-1/task3.py
+++ tasks-1/task3.py
@@ -2,8 +2,8 @@ counter = 0
 n = 1000
 
 # output = 233168
-l1 = list(range(3, n+1, 3))
-l2 = list(range(5, n, 5))
-l3 = list(range(15, n, 15))      
+l1 = range(3, n+1, 3)
+l2 = range(5, n, 5)
+l3 = range(15, n, 15)      
 
 print(sum(l1) + sum(l2) - sum(l3))
