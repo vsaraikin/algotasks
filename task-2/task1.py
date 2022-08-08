02f1f07a1724e3b122a1db5054437081ddc1d588
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-08-08 18:10:52 +0300

    partly solved task 2.1

---
+++ task-2/task1.py
@@ -0,0 +1,38 @@
+def first_divisor(n):
+    for i in range(2, n+1):
+        if n % i == 0:
+            return i
+
+def get_radical(n):
+    l = {first_divisor(n)}
+    def radicalize(n):
+        for i in range(max(l), 100000): 
+            if n % i == 0:
+                l.add(i)
+                n = n / i 
+                break
+        if n == 1:
+            return l
+        else:
+            return radicalize(n)
+    res = radicalize(n)
+    product = 1
+
+    for item in res:
+        product = product * item
+    
+    return product
+
+def generate_radical_series(N):
+    radicals_store = {}
+    for i in range(2, N + 1):
+        radicals_store[i] = get_radical(i)
+        
+    radicals_store = {k: v for k, v in sorted(radicals_store.items(), key=lambda item: item[1])}
+    return radicals_store
+
+def get_K_value(K):
+    store = generate_radical_series(10)
+    return list(store.keys())[K-2]
+        
+print(get_K_value(6))
