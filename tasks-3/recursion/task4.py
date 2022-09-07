0aacdd6203012290823e41ba0b1a475cca3bdfa3
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-09-07 19:21:52 +0300

    updated task3.4 with recursion

--- tasks-3/recursion/task4.py
+++ tasks-3/recursion/task4.py
@@ -1,17 +1,26 @@
-n, m = 0, 2
-line = [1, 3, 4]
+line = [1, 3, 4, 1, 2]
+n, m = 0, len(line) - 1
+
+
 first, second = [], []
 
 def func(n, m):
     first.append(max(line[n], line[m]))
-    second.append(min(line[n], line[m]))
+    second.append(min(line[n + 1], line[m - 1]))
+    
     n += 1
     m -= 1
-    if abs(m - n) in (0, 1):
-        first.append(n)
+    
+    if abs(m - n) == 0:
+        first.append(min(line[n + 1], line[n]))
+        
         # print(first, second)
-        return sum(first)
+    elif abs(m - n) == 1:
+        first.append(max(line[n + 1], line[n]))
+        second.append(min(line[n + 1], line[n]))
+        # print(first, second)
     else:
         func(n, m)
         
-print(func(n, m))
+func(n, m)
+print(sum(first))
