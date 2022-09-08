925d9ca233716a629283e5693d03c4c4ed0bad4d
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-09-08 13:07:57 +0300

    anotheer solution for task3.4

--- tasks-3/recursion/task4.py
+++ tasks-3/recursion/task4.py
@@ -1,31 +1,26 @@
-line = [2, 3, 100, 10, 1, 3]
+line = [3, 3, 3, 0, 1, 1, 1]
 n, m = 0, len(line) - 1
 
 
 first, second = [], []
 
 def func(n, m):
-    if line.index(max(line[n + 1], line[m - 1])) - n == 1:
-        first.append(line[m])
-        second.append(line[n])
-    else:
-        first.append(line[m])
-        second.append(line[n])
-    
+    first.append(max(line[n], line[m]))
+    second.append(min(line[n], line[m]))
     
     n += 1
     m -= 1
     
-    if abs(m - n) == 0:
-        first.append(min(line[n + 1], line[n]))
-        
-        print(first, second)
-    elif abs(m - n) == 1:
-        first.append(max(line[n], line[n + 1]))
-        second.append(min(line[n], line[n + 1]))
-        print(first, second)
+    if abs(m - n) == 1:
+        first.append(max(line[n], line[m]))
+        second.append(min(line[n], line[m]))
+        print(1)
+    elif abs(m - n) == 0:
+        first.append(line[n])
+        print(0)
+
     else:
         func(n, m)
         
 func(n, m)
-print(first)
+print(first, second)
