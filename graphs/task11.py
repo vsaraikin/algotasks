3b1eb3271cc8e028f380753152018897164ac489
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-10-26 08:55:32 +0300

    solved 11 in graphs

---
+++ graphs/task11.py
@@ -0,0 +1,31 @@
+A = [[0, 1, 0],
+     [1, 0, 1],
+     [0, 1, 0]]
+
+d = {}
+
+"""
+The easiest way:
+|V| = |E| + 1 
+"""
+
+def func1(A):
+    e = 0
+    v = len(A)
+    
+    for i in range(0, len(A)):
+        for j in range(i, len(A)):
+            if A[i][j] == 1:
+                e += 1
+    
+    if e + 1 == v:
+        return "Yes"
+    else:
+        return "No"
+
+print(func1(A))                
+
+
+
+
+
