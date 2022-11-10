c2822cf73f8c3e0e32f10c7f9be281ab2dc29784
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-11-10 16:43:10 +0100

    updated dfs in graphs

--- graphs/task9.py
+++ graphs/task9.py
@@ -1,3 +1,5 @@
+n = 7
+
 A = [
     [0, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 0, 0],
@@ -8,16 +10,29 @@ A = [
     [0, 0, 0, 0, 0, 1, 0]
 ]
 
-    
-visited = []
-
+visited = set() # for checking visited peaks O(1)
+visited_list = [] # for displaying visited peaks 
 
 def dfs(node):
-
-    visited.append(node)
-    for i in range(len(A)):
-        if A[node][i] == 1 and i not in visited:
-            dfs(i)
+    if node not in visited:
+        
+        visited.add(node)
+        visited_list.append(node + 1)
+        
+        slice = A[node]
+        
+        for i in range(n):
+            if slice[i] == 1:
+                dfs(i)
             
 dfs(0)
-print([x + 1 for x in visited])
+
+all_peaks = set(range(0, n)) 
+
+# there are peaks that did not have path to them, but we still should include them
+not_included = all_peaks - visited 
+
+for i in not_included:
+    visited_list.append(i + 1)
+    
+print(visited_list)
