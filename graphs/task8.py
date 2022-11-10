8f1920724182e2023d46dbc51bfea759325e5e40
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-11-10 14:38:40 +0100

    fixed 8 in graphs

--- graphs/task8.py
+++ graphs/task8.py
@@ -8,7 +8,7 @@ A = [
     [0, 0, 0, 0, 0, 1, 0]
 ]
 
-visited = []
+visited = set()
 
 def bfs():
 
@@ -17,9 +17,9 @@ def bfs():
             if A[i][x] == 1:
                 
                 if i + 1 not in visited:  
-                    visited.append(i + 1)
+                    visited.add(i + 1)
                 if x + 1 not in visited: 
-                    visited.append(x + 1)
+                    visited.add(x + 1)
             
 bfs()            
 print(visited)
