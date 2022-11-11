77d9ab4cc84ef93ede8e7d0666e8b2c32549a53a
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-11-11 13:43:49 +0100

    fixed 8 in graphs

--- graphs/task8.py
+++ graphs/task8.py
@@ -1,25 +1,56 @@
+n = 7
+
+# A = [
+#     [0, 1, 1, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 1, 0, 0],
+#     [0, 1, 0, 1, 1, 0, 0],
+#     [0, 1, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 0, 1, 0],
+#     [0, 0, 0, 1, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 1, 0]
+# ]
+
+n = 4
 A = [
-    [0, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 1, 0, 0],
-    [0, 1, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0]
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+    [0, 1, 0, 0]
 ]
 
+# import queue
 visited = set()
+visited_list = []
+queue = []
 
-def bfs():
-
-    for i in range(len(A)):
-        for x in range(len(A)):        
-            if A[i][x] == 1:
+def bfs(node):
+    visited.add(node)
+    visited_list.append(node)
+    queue.append(node)
+    
+    while queue:
+        tmp = queue.pop(0)
+        
+        slice = A[tmp]
+        for i in range(n):
+            if slice[i] == 1 and i not in visited:
+                visited.add(i)
+                visited_list.append(i)
+                queue.append(i)
                 
-                if i + 1 not in visited:  
-                    visited.add(i + 1)
-                if x + 1 not in visited: 
-                    visited.add(x + 1)
-            
-bfs()            
-print(visited)
+
+bfs(0)
+
+all_peaks = set(range(0, n)) 
+
+# there are peaks that did not have path to them, but we still should include them
+not_included = all_peaks - visited
+
+for i in not_included:
+    visited_list.append(i)
+    
+    
+visited_list = [x + 1 for x in visited_list]
+print(visited_list)
+
+                
