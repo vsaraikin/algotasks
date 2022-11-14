057b93a31aaa47ed07ba9a832380e0e731bd638c
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-11-14 16:09:09 +0100

    fixed 10 in graphs

--- graphs/task10.py
+++ graphs/task10.py
@@ -32,57 +32,37 @@ if n == 0 or m == 0:
 
 
 nodes = {x[0] for x in graph}.union({x[1] for x in graph}) # finding all of the verticles in graph
-edges = {(k[0], k[1]):k[2] for k in graph}
+
+import heapq
+from collections import defaultdict
+
+def dijkstra(edges, start=1):
     
-
-def find_best_clones(graph):
-    unique_graph = {}
-
-    for i in range(len(graph)):
-
-        if unique_graph.get((graph[i][0], graph[i][1])) is None:
-            unique_graph[(graph[i][0], graph[i][1])] = graph[i][2]
-
-        else:
-            current_value = unique_graph[(graph[i][0], graph[i][1])]
-            if graph[i][2] < current_value:
-                unique_graph[(graph[i][0], graph[i][1])] = current_value
-        
-    unique_graph = [(*x[0], x[1]) for x in unique_graph.items()]
+    # Changing data structure from 2-d list to dict
+    graph = {}
+    
+    for edge in edges:
+        if graph.get(edge[0]) is None:
+            graph[edge[0]] = []
             
-    return unique_graph
-
-# there is a corner case, when there are several connections with the same start and end points
-# we have to choose the smallest of them
-if len(edges) != len(graph):
-    graph = find_best_clones(graph)
-    edges = {(k[0], k[1]):k[2] for k in graph}
-
-
-
-def dijkstra(nodes, edges, start=1):
-    """ Shortest path """
+        graph[edge[0]].append((edge[1], edge[2]))
     
-    path_lengths = {v:float('inf') for v in nodes}
-    path_lengths[start] = 0
+    path_lengths = {}
+    heap = [(0, start)]
     
-    adj_nodes = {v:{} for v in nodes}
-    
-    for (u, v), weight in edges.items():
-        adj_nodes[u][v] = weight
-        adj_nodes[v][u] = weight
+    while heap:
+        dist, node = heapq.heappop(heap)
         
-    temp_nodes = [v for v in nodes]
-    
-    while len(temp_nodes) > 0:
-        upper_bounds = {v: path_lengths[v] for v in temp_nodes}
-        u = min(upper_bounds, key=upper_bounds.get)
-        temp_nodes.remove(u)
+        if node in path_lengths:
+            continue
         
-        for v, weight in adj_nodes[u].items():
-            path_lengths[v] = min(path_lengths[v], path_lengths[u] + weight)
-            
+        path_lengths[node] = dist
+        
+        if graph.get(node):
+            for end_element, weight in graph[node]:
+                if end_element not in path_lengths:
+                    heapq.heappush(heap, (dist + weight, end_element))
+                
     return path_lengths[max(nodes)]
-    
-    
-print(dijkstra(nodes, edges))
+
+print(dijkstra(graph))
