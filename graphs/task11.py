28ffbcf572726ab164f640f59b85813db84e89f7
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-11-14 10:45:43 +0100

    fixed 11 in graphs

--- graphs/task11.py
+++ graphs/task11.py
@@ -1,6 +1,9 @@
-A = [[0, 1, 0],
-     [1, 0, 1],
-     [0, 1, 0]]
+verticles = 4
+
+A = [[0, 1, 1, 0],
+    [1, 0, 1, 0],
+    [1, 1, 0, 0],
+    [0, 0, 0, 0]]
 
 d = {}
 
@@ -13,7 +16,11 @@ def func1(A):
     e = 0
     v = len(A)
     
+    
     for i in range(0, len(A)):
+        if A[i].count(0) == verticles:
+            return "No"
+        
         for j in range(i, len(A)):
             if A[i][j] == 1:
                 e += 1
