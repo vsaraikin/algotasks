65636c59cfdedfe69189e5c1f2c325e68fd0bf95
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-12-05 15:53:25 +0300

    boosted 110

--- leetcode/110.py
+++ leetcode/110.py
@@ -4,22 +4,39 @@ class TreeNode:
         self.left = left
         self.right = right
         
-        
+flag = True
+
+
 class Solution:
+    
+    def __init__(self):
+        self.m2 = 1
 
     def maxDepth(self,root):
         if root is None or isinstance(root, int):
             return 0
         else:
-            return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+            res = 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+            
+            if res > self.m2:
+                self.m2 = res
+            
+            if self.m2 - res >= 2:
+                flag = False
+            
+            return res
         
 
     def isBalanced(self, root: TreeNode) -> bool:
+        if not flag:
+            return False
+        
         if root is None:
             return True
 
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
+        
         
         if left - right > 1 or right - left > 1:
             return False
@@ -40,8 +57,14 @@ class Solution:
 root = TreeNode(3)
 root.left = TreeNode(9)
 root.right = TreeNode(20)
+# root.left.left = TreeNode(3)
+# root.left.right = TreeNode(3)
+# root.left.left.left = TreeNode(3)
+# root.left.left.right = TreeNode(3)
 root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
+root.right.right.right = TreeNode(9)
+
 
 s = Solution()
 print(s.isBalanced(root))
